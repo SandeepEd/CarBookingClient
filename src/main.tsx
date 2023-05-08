d762c096648d3e6fa626c52c498633c650a2eef0
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-08 00:14:36 -0400

    remove CssBaseline and add AppProviders

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-import { CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { AuthProvider } from './context/AuthContext.tsx';
+import Entry from './pages/auth/Entry.tsx';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CssBaseline>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </CssBaseline>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Entry />
+        </AuthProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
