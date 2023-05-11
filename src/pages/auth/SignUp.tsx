45f5501795328c1046f9f72ada2cb84d0a09178f
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-10 20:16:25 -0400

    add name, remove conform password

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,12 +1,18 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material"
 import { useForm } from "react-hook-form";
+import { AuthService } from "../../services/AuthService";
+import { IUser } from "../../types/Auth";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
 
-    const { register, handleSubmit } = useForm();
+    const navigate = useNavigate();
 
-    const onSubmit = (data: any) => {
-        console.log('Submitted:', data);
+    const { register, handleSubmit } = useForm<IUser>();
+
+    const onSubmit = async (data: IUser) => {
+        await AuthService.signUp(data)
+        navigate('/login');
     };
     return (
         <Container maxWidth="xs">
@@ -22,6 +28,16 @@ function SignUp() {
                     Sign Up
                 </Typography>
                 <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        {...register("name", { required: true })}
+                        fullWidth
+                        id="name"
+                        label="Name"
+                        name="name"
+                        autoComplete="name"
+                        autoFocus
+                    />
                     <TextField
                         margin="normal"
                         {...register("email", { required: true })}
@@ -40,16 +56,6 @@ function SignUp() {
                         label="Password"
                         type="password"
                         id="password"
-                        autoComplete="new-password"
-                    />
-                    <TextField
-                        margin="normal"
-                        {...register("confirmPassword", { required: true })}
-                        fullWidth
-                        name="confirmPassword"
-                        label="Confirm Password"
-                        type="password"
-                        id="confirmPassword"
                         autoComplete="new-password"
                     />
                     <Button
