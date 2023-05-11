c550083ca56fb6dbae0dcc0a0c465b4a241a2fdf
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-10 20:16:54 -0400

    signUp and getSession

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,10 +1,20 @@
 import client from "../utils/http";
 import { IUser } from "../types/Auth";
+import cookie from 'cookie'
 
 export class AuthService {
     static login(creds: IUser) {
         return client.post('/login', creds)
     }
+
+    static signUp(creds: IUser) {
+        return client.post('/sign-up', creds)
+    }
 }
 
 export const login = async (creds: IUser): Promise<IUser> => await client.post('/login', creds)
+
+export const getSession = () => {
+    const { spa_token } = cookie.parse(document.cookie);
+    return spa_token;
+}
