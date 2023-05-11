4d010624f686f41ee58690d21b37679baa44e238
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-10 20:17:42 -0400

    withCredentials true for cookie transfer over network

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const client = axios.create({
     baseURL: 'http://localhost:3000',
+    withCredentials: true,
     headers: {
         'Content-Type': `application/json`,
     },
