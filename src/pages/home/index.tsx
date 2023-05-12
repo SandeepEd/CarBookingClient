00ffcceb17fd68728b07d3599b0f2cef636e4f6f
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-12 19:10:22 -0400

    edits to the home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,35 @@
+import React from 'react';
+import { Button, Typography, Container, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-function Home() {
+const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    const navigateToCars = () => {
+        navigate('/cars');
+    }
+
     return (
-        <div>Welcome to home page!</div>
-    )
-}
+        <Container>
+            <Typography variant="h2" align="center" gutterBottom>
+                Welcome to Our Car Buying Website!
+            </Typography>
+            <Typography variant="h6" align="center">
+                We offer a wide range of cars for all budgets and preferences.
+            </Typography>
+            <Box display="flex" justifyContent="center" marginTop={2}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    onClick={navigateToCars}
+                    style={{ marginTop: '20px' }}
+                >
+                    View Available Cars
+                </Button>
+            </Box>
+        </Container>
+    );
+};
 
-export default Home
+export default Home;
